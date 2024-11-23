bbb4269fd7f22b68e47393ce911828a8040f4193
gogodoesnotcode <giteshnaidu7@gmail.com>
2024-11-24 00:47:55 +0530

    udoshikimkc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useRef } from "react";
-import udoshiImage from "./Asset/udoshi.jpg"; // Import the background image
+import React, { useEffect, useRef } from "react";
+import udoshiImage from "./Asset/alshaj.jpg"; // Import the background image
 import giteshSound from "./Asset/gitesh.mp3"; // Import the audio file
 
 const App = () => {
   const audioRef = useRef(null);
 
-  const handleButtonClick = () => {
+  useEffect(() => {
     if (audioRef.current) {
-      audioRef.current.play();
+      audioRef.current.play().catch((err) => {
+        console.error("Failed to play audio:", err);
+      });
     }
-  };
+  }, []);
 
   return (
     <div
@@ -19,26 +21,9 @@ const App = () => {
         backgroundImage: `url(${udoshiImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <button
-        onClick={handleButtonClick}
-        style={{
-          padding: "10px 20px",
-          fontSize: "20px",
-          borderRadius: "5px",
-          border: "none",
-          backgroundColor: "#333",
-          color: "#fff",
-          cursor: "pointer",
-        }}
-      >
-        Bruh
-      </button>
-      <audio ref={audioRef}>
+      <audio ref={audioRef} loop>
         <source src={giteshSound} type="audio/mpeg" />
       </audio>
     </div>
